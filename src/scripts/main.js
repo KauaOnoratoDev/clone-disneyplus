6ad6b66292bec31cf617144063400602a086366a
KauaOnorato <kauaonorato.dev@gmail.com>
2023-12-27 20:48:38 -0400

    configurando uglify

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
     const heroSection = document.querySelector('.hero').clientHeight;
+    
 
     window.addEventListener('scroll', function() {
         if (window.scrollY < heroSection) {
